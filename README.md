d064c81022aefbe9faff7f5faa1a850a293ba5a7
Alex <alexanderthestudent@yandex.ru>
2025-02-09 19:58:07 +0300

    feat: terminal working

---
+++ README.md
@@ -0,0 +1,7 @@
+## Work in progress lab platform
+
+All files except `index.html` are taken from [copy's v86](https://github.com/copy/v86)
+which is under the [Simplified BSD License](https://github.com/copy/v86/blob/master/LICENSE).
+
+Some files are modified
+
---
+++ images/build-state.js
@@ -0,0 +1,57 @@
+#!/usr/bin/env node
+"use strict";
+
+const path = require("path");
+const fs = require("fs");
+// three .. up is just ../ in my case
+const V86 = require("./../libv86.js").V86;
+
+const V86_ROOT = path.join(__dirname, "..");
+const OUTPUT_FILE = path.join(V86_ROOT, "images/dist/alpine-state.bin");
+
+var emulator = new V86({
+    bios: { url: path.join(V86_ROOT, "seabios.bin") },
+    vga_bios: { url: path.join(V86_ROOT, "vgabios.bin") },
+    autostart: true,
+    memory_size: 512 * 1024 * 1024,
+    vga_memory_size: 8 * 1024 * 1024,
+    network_relay_url: "<UNUSED>",
+    bzimage_initrd_from_filesystem: true,
+    cmdline: "rw root=host9p rootfstype=9p rootflags=trans=virtio,cache=loose modules=virtio_pci tsc=reliable init_on_free=on",
+    filesystem: {
+        baseurl: path.join(V86_ROOT, "images/dist/alpine-rootfs-flat"),
+        basefs: path.join(V86_ROOT, "images/dist/alpine-fs.json"),
+    },
+});
+
+console.log("Now booting, please stand by ...");
+
+let serial_text = "";
+let booted = false;
+
+emulator.add_listener("serial0-output-byte", function(byte)
+{
+    const c = String.fromCharCode(byte);
+    process.stdout.write(c);
+
+    serial_text += c;
+
+    if(!booted && serial_text.endsWith("localhost:~# "))
+    {
+        booted = true;
+
+        emulator.serial0_send("sync;echo 3 >/proc/sys/vm/drop_caches\n");
+
+        setTimeout(async function ()
+            {
+                const s = await emulator.save_state();
+
+                fs.writeFile(OUTPUT_FILE, new Uint8Array(s), function(e)
+                    {
+                        if(e) throw e;
+                        console.log("Saved as " + OUTPUT_FILE);
+                        emulator.destroy();
+                    });
+            }, 10 * 1000);
+    }
+});
